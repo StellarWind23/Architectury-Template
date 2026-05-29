f38f4616126144c54ebb9894a459fc03ab4623d8
stellarwind23 <stellarwind@pm.me>
2026-05-29 17:01:25 -0500

    fix each `build.gradle`

--- common/src/main/java/com/stellarwind23/template/init/TemplateInit.java
+++ common/src/main/java/com/stellarwind23/template/init/TemplateInit.java
@@ -1,7 +1,12 @@
 package com.stellarwind23.template.init;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class TemplateInit {
+
     public static final String MOD_ID = "${modid}";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
         // Write common init code here.
