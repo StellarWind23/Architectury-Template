7972fe5b7edfaaa6f2db436cbe3612f4fa776729
stellarwind23 <stellarwind@pm.me>
2026-05-29 17:46:43 -0500

    fine modid is in two places now >:(

--- common/src/main/java/com/github/stellarwind23/template/init/TemplateInit.java
+++ common/src/main/java/com/github/stellarwind23/template/init/TemplateInit.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public final class TemplateInit {
 
-    public static final String MOD_ID = "${modid}";
+    public static final String MOD_ID = "template";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
